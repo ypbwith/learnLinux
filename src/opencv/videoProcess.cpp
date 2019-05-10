873289c0b1f0f457fa2af20aff99ad4ea7121946
杨蓬勃 <yangpengbo2636@gmail.com>
2019-05-10 00:23:11 -0700

    添加opencv编译命令

---
+++ src/opencv/videoProcess.cpp
@@ -0,0 +1,5 @@
+//
+// Created by root on 5/9/19.
+//
+
+#include "videoProcess.h"
---
+++ src/opencv/videoProcess.h
@@ -0,0 +1,14 @@
+//
+// Created by root on 5/9/19.
+//
+
+#ifndef LEARNLINUX_VIDEOPROCESS_H
+#define LEARNLINUX_VIDEOPROCESS_H
+
+
+class videoProcess {
+
+};
+
+
+#endif //LEARNLINUX_VIDEOPROCESS_H
